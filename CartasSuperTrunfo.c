f1ea9625363f2e8873dfd6ba7267351c31ec7570
weasabrii <sabrina.mirandaa59@gmail.com>
2025-04-06 12:29:18 +0000

    Novo Comit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+int main(){
+    print("Desafio Xadrez!\n");
+    
 
+
+
+
+
+
+
+
+}
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
